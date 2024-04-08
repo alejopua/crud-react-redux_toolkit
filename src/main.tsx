59907340a5e672ed0b5c3372ad6ae66a04bab9da
alejopua <alejopua@gmail.com>
2024-04-08 17:44:22 -0500

    Refactor main.tsx to remove unused variables and import index.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
-
-const a = 1
-const b = 2
-const c = 4
-const d = 5
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
